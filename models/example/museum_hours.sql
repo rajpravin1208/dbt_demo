8bfcb973ecac4fd15262edfe1542d51a526cb36c
Raj Pravin Rajendran <rarajendran@libertyglobal.com>
2024-05-03 06:28:34 +0000

    new

---
+++ models/example/museum_hours.sql
@@ -0,0 +1,10 @@
+
+SELECT
+        museum_id AS Museum_ID,
+        day AS Day,
+        open AS Opening_Time,
+        close AS Closing_Time
+
+FROM `prj-data-d-poc-5-7joi.SQL_demo.museum_hours`
+
+ORDER BY 1
