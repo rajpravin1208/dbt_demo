6fe02fb05d8ddbbed47f7e08f1bed50b71fb71c1
Raj Pravin Rajendran <rarajendran@libertyglobal.com>
2024-05-15 07:23:16 +0000

    dimension

--- models/example/Dimproduct.sql
+++ models/example/Dimproduct.sql
@@ -1,16 +1,14 @@
-With new_data as
-
-(SELECT 
-  Distinct ProductID,
-  Product,
-  Category,
-  Segment,
-  Unit_Cost,
-  Unit_Price
-FROM {{ ref('Sales') }} )
-
-
-
+WITH new_data AS (
+  SELECT 
+    DISTINCT ProductID,
+    Product,
+    Category,
+    Segment,
+    Unit_Cost,
+    Unit_Price
+  FROM {{ ref('Sales') }}
+),
+C AS (
   SELECT 
     ROW_NUMBER() OVER(ORDER BY ProductID) AS ProductSK,
     ProductID,
@@ -20,3 +18,9 @@ FROM {{ ref('Sales') }} )
     Unit_Cost,
     Unit_Price
   FROM new_data
+)
+SELECT C.*, d.CatsegID
+FROM C
+LEFT JOIN `prj-data-d-poc-5-7joi`.`dbt_demo_bq`.`DimCatSeg` AS d
+  ON d.Segment = C.Segment
+  AND d.Category = C.Category
